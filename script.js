cacf198230581faebd7b2302e1cc7798794c0a03
ATIK <atikul2585@gmail.com>
2023-12-13 11:33:08 +0600

    seventh commit

--- script.js
+++ script.js
@@ -69,3 +69,41 @@ document.addEventListener("DOMContentLoaded", () => {
                 forecastHTML += `</div><div class="five-day-forecast-grid">`;
             }
         });
+
+        forecastHTML += `</div>`;
+    fiveDayForecastContainer.innerHTML = forecastHTML;
+}
+
+    async function getWeatherData(location) {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
+            );
+            if (!response.ok) {
+                throw new Error("Weather data not available for the given location.");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching weather data:", error);
+            return null;
+        }
+    }
+
+    async function getForecastData(location) {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=${apiKey}`
+            );
+            if (!response.ok) {
+                throw new Error("Forecast data not available for the given location.");
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error("Error fetching forecast data:", error);
+            return null;
+        }
+    }
+
+    
